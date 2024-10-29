rhel-lightspeed/shellai

Rework config file handler and history
Introduced two new modules, one for handling code related to config files
The second, to handle history where we read and append to new history.
Dropped the support for pypyaml as well. We will move on with tomllib

shellai/__main__.py
@@ -3,12 +3,16 @@ import logging
 import os
 import sys
 
+from shellai.config import (
+    CONFIG_DEFAULT_PATH,
+    load_config_file,
+)
 from shellai.handlers import (
     handle_history_write,
     handle_query,
     handle_script_session,
 )
-from shellai.utils import read_stdin, read_yaml_config
+from shellai.utils import read_stdin
 
 logging.basicConfig(
     level=logging.INFO,
@@ -32,7 +36,7 @@ def get_args():
     )
     parser.add_argument(
         "--config",
-        default=os.getenv("SHELLAI_CONFIG", "config.yaml"),
+        default=CONFIG_DEFAULT_PATH,
         help="Path to the config file.",
     )
 
@@ -57,15 +61,10 @@ def get_args():
 def main():
     parser, args = get_args()
 
-    config = read_yaml_config(args.config)
-    if not config:
-        logging.warning(
-            "Config file not found. Script will continue with default values."
-        )
+    config = load_config_file(args.config)
 
-    output_capture_conf = config.get("output_capture", {})
-    enforce_script_session = output_capture_conf.get("enforce_script", False)
-    output_file = output_capture_conf.get("output_file", "/tmp/shellai_output.txt")
+    enforce_script_session = config.output.enforce_script
+    output_file = config.output.enforce_script
 
     if enforce_script_session and (not args.record or not os.path.exists(output_file)):
         parser.error(
@@ -79,7 +78,7 @@ def main():
         exit(0)
     if args.history_clear:
         logging.info("Clearing history of conversation")
-        handle_history_write(config.get("history", {}), [], "")
+        handle_history_write(config, [], "")
     if args.query_string:
         handle_query(args.query_string, config)
 

shellai/handlers.py
@@ -4,49 +4,9 @@ import os
 
 import requests
 
+from shellai.config import Config
+from shellai.history import handle_history_read, handle_history_write
 from shellai.utils import get_payload
-
-
-def _handle_history_read(config: dict) -> dict:
-    """
-    Reads the history from a file and returns it as a list of dictionaries.
-    """
-    if not config.get("enabled", False):
-        return []
-
-    filepath = config.get("filepath", "/tmp/shellai_history.json")
-    if not filepath or not os.path.exists(filepath):
-        logging.warning(f"History file {filepath} does not exist.")
-        logging.warning("File will be created with first response.")
-        return []
-
-    max_size = config.get("max_size", 100)
-    history = []
-    try:
-        with open(filepath, "r") as f:
-            history = json.load(f)
-    except json.JSONDecodeError as e:
-        logging.error(f"Failed to read history file {filepath}: {e}")
-        return []
-
-    logging.info(f"Taking maximum of {max_size} entries from history.")
-    return history[:max_size]
-
-
-def handle_history_write(config: dict, history: list, response: str) -> None:
-    """
-    Writes the history to a file.
-    """
-    if not config.get("enabled", False):
-        return
-    filepath = config.get("filepath", "/tmp/shellai_history.json")
-    if response:
-        history.append({"role": "assistant", "content": response})
-    try:
-        with open(filepath, "w") as f:
-            json.dump(history, f)
-    except json.JSONDecodeError as e:
-        logging.error(f"Failed to write history file {filepath}: {e}")
 
 
 def handle_script_session(shellai_tmp_file) -> None:
@@ -65,17 +25,14 @@ def handle_script_session(shellai_tmp_file) -> None:
         os.remove(shellai_tmp_file)
 
 
-def _handle_caret(query: str, config: dict) -> str:
+def _handle_caret(query: str, config: Config) -> str:
     """
     Replaces caret (^) with command output specified in config file.
     """
     if "^" not in query:
         return query
 
-    output_capture_settings = config.get("output_capture_settings", {})
-    captured_output_file = output_capture_settings.get(
-        "captured_output_file", "/tmp/shellai_output.txt"
-    )
+    captured_output_file = config.output.file
 
     if not os.path.exists(captured_output_file):
         logging.error(
@@ -83,36 +40,33 @@ def _handle_caret(query: str, config: dict) -> str:
         )
         exit(1)
 
-    prompt_separator = output_capture_settings.get("prompt_separator", "$")
+    prompt_separator = config.output.prompt_separator
     with open(captured_output_file, "r") as f:
         # NOTE: takes only last command + output from file
         output = f.read().split(prompt_separator)[-1].strip()
+
     query = query.replace("^", "")
     query = f"Context data: {output}\nQuestion: " + query
     return query
 
 
-def handle_query(query: str, config: dict) -> None:
+def handle_query(query: str, config: Config) -> None:
     query = _handle_caret(query, config)
     # NOTE: Add more query handling here
 
     logging.info(f"Query: {query}")
 
-    backend_service = config.get("backend_service", {})
-    query_endpoint = backend_service.get(
-        "query_endpoint", "http://0.0.0.0:8080/v1/query/"
-    )
+    query_endpoint = config.backend.endpoint
 
     try:
-        history_conf = config.get("history", {})
-        history = _handle_history_read(history_conf)
+        history = handle_history_read(config)
         payload = get_payload(query)
         logging.info("Waiting for response from AI...")
         response = requests.post(
             query_endpoint,
             headers={"Content-Type": "application/json"},
             data=json.dumps(payload),
-            timeout=30,  # waiting for more than 30 seconds does not make sense
+            timeout=320,  # waiting for more than 30 seconds does not make sense
         )
         response.raise_for_status()
         completion = response.json()
@@ -125,7 +79,7 @@ def handle_query(query: str, config: dict) -> None:
             "\n\nReferences:\n" + "\n".join(references) if references else ""
         )
         handle_history_write(
-            history_conf,
+            config,
             [
                 *history,
                 {"role": "user", "content": query},

rhel-lightspeed/command-line-assistant

command_line_assistant/__main__.py
@@ -3,12 +3,13 @@ import logging
 import os
 import sys
 
+from command_line_assistant.config import CONFIG_DEFAULT_PATH, load_config_file
 from command_line_assistant.handlers import (
     handle_history_write,
     handle_query,
     handle_script_session,
 )
-from command_line_assistant.utils import read_stdin, read_yaml_config
+from command_line_assistant.utils import read_stdin
 
 logging.basicConfig(
     level=logging.INFO,
@@ -32,7 +33,7 @@ def get_args():
     )
     parser.add_argument(
         "--config",
-        default=os.getenv("COMMAND_LINE_ASSISTANT_CONFIG", "config.yaml"),
+        default=CONFIG_DEFAULT_PATH,
         help="Path to the config file.",
     )
 
@@ -57,17 +58,10 @@ def get_args():
 def main():
     parser, args = get_args()
 
-    config = read_yaml_config(args.config)
-    if not config:
-        logging.warning(
-            "Config file not found. Script will continue with default values."
-        )
+    config = load_config_file(args.config)
 
-    output_capture_conf = config.get("output_capture", {})
-    enforce_script_session = output_capture_conf.get("enforce_script", False)
-    output_file = output_capture_conf.get(
-        "output_file", "/tmp/command-line-assistant_output.txt"
-    )
+    enforce_script_session = config.output.enforce_script
+    output_file = config.output.enforce_script
 
     if enforce_script_session and (not args.record or not os.path.exists(output_file)):
         parser.error(
@@ -81,7 +75,7 @@ def main():
         exit(0)
     if args.history_clear:
         logging.info("Clearing history of conversation")
-        handle_history_write(config.get("history", {}), [], "")
+        handle_history_write(config, [], "")
     if args.query_string:
         handle_query(args.query_string, config)
 

command_line_assistant/handlers.py
@@ -4,49 +4,9 @@ import os
 
 import requests
 
+from command_line_assistant.config import Config
+from command_line_assistant.history import handle_history_read, handle_history_write
 from command_line_assistant.utils import get_payload
-
-
-def _handle_history_read(config: dict) -> dict:
-    """
-    Reads the history from a file and returns it as a list of dictionaries.
-    """
-    if not config.get("enabled", False):
-        return []
-
-    filepath = config.get("filepath", "/tmp/command-line-assistant_history.json")
-    if not filepath or not os.path.exists(filepath):
-        logging.warning(f"History file {filepath} does not exist.")
-        logging.warning("File will be created with first response.")
-        return []
-
-    max_size = config.get("max_size", 100)
-    history = []
-    try:
-        with open(filepath, "r") as f:
-            history = json.load(f)
-    except json.JSONDecodeError as e:
-        logging.error(f"Failed to read history file {filepath}: {e}")
-        return []
-
-    logging.info(f"Taking maximum of {max_size} entries from history.")
-    return history[:max_size]
-
-
-def handle_history_write(config: dict, history: list, response: str) -> None:
-    """
-    Writes the history to a file.
-    """
-    if not config.get("enabled", False):
-        return
-    filepath = config.get("filepath", "/tmp/command-line-assistant_history.json")
-    if response:
-        history.append({"role": "assistant", "content": response})
-    try:
-        with open(filepath, "w") as f:
-            json.dump(history, f)
-    except json.JSONDecodeError as e:
-        logging.error(f"Failed to write history file {filepath}: {e}")
 
 
 def handle_script_session(command_line_assistant_tmp_file) -> None:
@@ -65,17 +25,14 @@ def handle_script_session(command_line_assistant_tmp_file) -> None:
         os.remove(command_line_assistant_tmp_file)
 
 
-def _handle_caret(query: str, config: dict) -> str:
+def _handle_caret(query: str, config: Config) -> str:
     """
     Replaces caret (^) with command output specified in config file.
     """
     if "^" not in query:
         return query
 
-    output_capture_settings = config.get("output_capture_settings", {})
-    captured_output_file = output_capture_settings.get(
-        "captured_output_file", "/tmp/command-line-assistant_output.txt"
-    )
+    captured_output_file = config.output.file
 
     if not os.path.exists(captured_output_file):
         logging.error(
@@ -83,36 +40,33 @@ def _handle_caret(query: str, config: dict) -> str:
         )
         exit(1)
 
-    prompt_separator = output_capture_settings.get("prompt_separator", "$")
+    prompt_separator = config.output.prompt_separator
     with open(captured_output_file, "r") as f:
         # NOTE: takes only last command + output from file
         output = f.read().split(prompt_separator)[-1].strip()
+
     query = query.replace("^", "")
     query = f"Context data: {output}\nQuestion: " + query
     return query
 
 
-def handle_query(query: str, config: dict) -> None:
+def handle_query(query: str, config: Config) -> None:
     query = _handle_caret(query, config)
     # NOTE: Add more query handling here
 
     logging.info(f"Query: {query}")
 
-    backend_service = config.get("backend_service", {})
-    query_endpoint = backend_service.get(
-        "query_endpoint", "http://0.0.0.0:8080/v1/query/"
-    )
+    query_endpoint = config.backend.endpoint
 
     try:
-        history_conf = config.get("history", {})
-        history = _handle_history_read(history_conf)
+        history = handle_history_read(config)
         payload = get_payload(query)
         logging.info("Waiting for response from AI...")
         response = requests.post(
             query_endpoint,
             headers={"Content-Type": "application/json"},
             data=json.dumps(payload),
-            timeout=30,  # waiting for more than 30 seconds does not make sense
+            timeout=320,  # waiting for more than 30 seconds does not make sense
         )
         response.raise_for_status()
         completion = response.json()
@@ -125,7 +79,7 @@ def handle_query(query: str, config: dict) -> None:
             "\n\nReferences:\n" + "\n".join(references) if references else ""
         )
         handle_history_write(
-            history_conf,
+            config,
             [
                 *history,
                 {"role": "user", "content": query},