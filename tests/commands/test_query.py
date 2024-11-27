d8b39c59d247e57f662d66541edac5ca61c95995
Rodolfo Olivieri <rolivier@redhat.com>
2024-11-27 14:26:08 -0300

    Refactor CLI to have separate commands
    This patch will introduce a refactor/split of our CLI module. The
    intention here is to make sure that each (sub)command will be treated
    independently and have their own logic.
    
    Currently, it is being divided into two categories:
    * history
    * query
    
    The history subcommand will handle everything that is related to the
    user history of conversation.
    
    The query subcommand will handle anything that is related to user
    queries. This is also the default subcommand.

---
+++ tests/commands/test_query.py
@@ -0,0 +1,39 @@
+from argparse import ArgumentParser, Namespace
+
+from dasbus.server.template import InterfaceTemplate
+from dasbus.typing import Structure
+
+from command_line_assistant.commands import query
+from command_line_assistant.dbus.definitions import MessageOutput
+
+
+def test_command_factory():
+    expected = "test"
+
+    instance = query._command_factory(Namespace(query_string="test"))
+
+    assert isinstance(instance, query.QueryCommand)
+    assert instance._query == expected
+
+
+def test_register_command():
+    parser = ArgumentParser()
+    sub_parser = parser.add_subparsers()
+
+    query.QueryCommand.register_subcommand(sub_parser)
+
+    parser.parse_args(["query", "test"])
+
+
+class GetProxyMock(InterfaceTemplate):
+    def __new__(cls):
+        return cls
+
+    def ProcessQuery(self) -> None:
+        return
+
+    @property
+    def RetrieveAnswer(self) -> Structure:
+        output = MessageOutput()
+        output.message = "hi"
+        return MessageOutput.to_structure(output)
