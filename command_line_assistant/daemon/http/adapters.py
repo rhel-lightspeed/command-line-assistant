65bed0f1aa54550d30de66060eafc20eab7b1ccd
Rodolfo Olivieri <rolivier@redhat.com>
2025-03-10 15:33:04 -0300

    Fix session ssl certs not being loaded properly
    We were using the SSLAdapters wrong this entire time. Now that we can
    test against staging, we saw that the certs were not being loaded
    correctly. This patch simplify this process to attach the certs to the
    session and let requests to handle it.

--- command_line_assistant/daemon/http/adapters.py
+++ command_line_assistant/daemon/http/adapters.py
@@ -1,41 +1,12 @@
 """Main module to track the adapters for the http backend."""
 
-import ssl
-from pathlib import Path
+import logging
 from typing import Union
 
 from requests.adapters import HTTPAdapter
 from urllib3 import Retry
 
-
-class SSLAdapter(HTTPAdapter):
-    """Create an adapter to use a custom SSL context in requests.
-
-    Attributes:
-        cert_file (Path): The path to the cert file on the system
-        key_file (Path): The path to the key file on the system
-        ssl_context: Any additional ssl context that needs to be used
-    """
-
-    def __init__(self, cert_file: Path, key_file: Path, ssl_context=None, **kwargs):
-        """Constructor of the class
-
-        Arguments:
-            cert_file (Path): The path to the cert file on the system
-            key_file (Path): The path to the key file on the system
-            ssl_context: Any additional ssl context that needs to be used. Defaults to None.
-        """
-        self.cert_file = cert_file
-        self.key_file = key_file
-        self.ssl_context = ssl_context or ssl.create_default_context()
-        super().__init__(**kwargs)
-
-    def init_poolmanager(self, *args, **kwargs) -> None:
-        """Initialize the poolmanager"""
-        # Load the certificate and key into the SSL context
-        self.ssl_context.load_cert_chain(certfile=self.cert_file, keyfile=self.key_file)
-        kwargs["ssl_context"] = self.ssl_context
-        super().init_poolmanager(*args, **kwargs)
+logger = logging.getLogger(__name__)
 
 
 class RetryAdapter(HTTPAdapter):
@@ -56,6 +27,7 @@ class RetryAdapter(HTTPAdapter):
             max_retries (Union[Retry, int, None], optional): The maximum number of retires. Defaults to 3.
             pool_block (bool, optional): If the pool should be blocked. Defaults to False.
         """
+        logger.info("Configuring retry adapter with max_retries of %s", max_retries)
         retries = Retry(
             total=max_retries,
             backoff_factor=0.1,
